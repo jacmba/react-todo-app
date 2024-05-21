84ae75032d3b5fc8b942f1ad2fba4c2b87ebbec5
Jacinto Mba Cantero <jacmba@gmail.com>
2024-05-21 10:57:48 +0100

    Call hello world api

--- src/components/todo/welcome/WelcomeComponent.jsx
+++ src/components/todo/welcome/WelcomeComponent.jsx
@@ -1,8 +1,26 @@
+import axios from "axios"
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 const WelcomeComponent = () => {
 
   const username = sessionStorage.getItem('user')
+
+  const [message, setMessage] = useState(null)
+
+  const callHelloApi = async () => {
+    const uri = 'http://localhost:8080/hello-world/path-variable/' +
+      username
+
+    try  {
+      const result = await axios.get(uri)
+      console.log(result)
+      setMessage(result.data.message)
+    } catch (e) {
+      console.error(e)
+      setMessage(null)
+    }
+  }
 
   return (
     <div className="Welcome">
@@ -10,6 +28,12 @@ const WelcomeComponent = () => {
       <div>
         <Link to="/todos">Manage your ToDos</Link>
       </div>
+      <div>
+        <button className="btn btn-success m-5" onClick={callHelloApi}>
+          Call Hello World
+        </button>
+      </div>
+      <div className="text-info">{message}</div>
     </div>
   )
 }
